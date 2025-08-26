52535460eff76f34e962518cf1b8d5969a4d428c
Thays-Cobra <thays.perez.13@gmail.com>
2025-08-26 18:39:22 -0300

    implementation querySelector

--- client/app/index.js
+++ client/app/index.js
@@ -1,3 +1,4 @@
+//armazenar referencia para cada um dos elementos de emtrada do formulario
 var campos = [
     document.querySelector('#data'),
     document.querySelector('#valor'),
@@ -5,3 +6,12 @@ var campos = [
 ];
 
 console.log(campos);
+
+
+//precisamos de tbody, pois ele receberá a tr que vamos construir
+
+var tbody = document.querySelector('table tbody');
+
+document.querySelector('.form').addEventListener('submit', function (event) {
+    alert('oi')
+});
