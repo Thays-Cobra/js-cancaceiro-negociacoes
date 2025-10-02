97881da67f03bad80fa9716b0e454c0d66862e13
Thays Cobra Perez <thays.perez.13@gmail.com>
2025-10-02 11:41:14 -0300

    refactor: organize code

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -2,7 +2,7 @@ class Negociacao {
 
     constructor(_data, _quantidade, _valor) {
         this._data = new Date(_data.getTime());
-        Object.assign(this, {_quantidade, _valor});
+        Object.assign(this, {_quantidade, _valor}); // shorthand property names
         Object.freeze(this);
     }
 
